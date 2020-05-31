44fa06deee233acbfd19af9fa403708b873c0cfb
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-05-31 19:47:02 +0530

    Created Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,11 @@
+/*
+Problem Link : https://www.hackerrank.com/challenges/welcome-to-java/problem
+*/
+
+//Solution :
+
+public class Solution {
+  public static void main(String[] args) {
+        System.out.println("Hello, World.\nHello, Java.");
+    }
+}
