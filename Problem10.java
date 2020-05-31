4d0d0126cb820a857c10e0ce73d34fd4fef1201a
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-05-31 22:39:10 +0530

    Created Problem10.java

---
+++ Problem10.java
@@ -0,0 +1,40 @@
+/*
+Problem Link : https://www.hackerrank.com/challenges/java-static-initializer-block/problem?h_r=next-challenge&h_v=zen
+*/
+
+// Solution :
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+static int B, H;
+static boolean flag;
+static Scanner s;
+static {
+    s = new Scanner(System.in);
+    B = s.nextInt();
+    H = s.nextInt();
+    try {
+        if( B<=0 || H<=0) {
+            flag = false;
+            throw new Exception("Breadth and height must be positive");
+        }
+        flag = true;
+    }
+    catch(Exception e) {
+        System.out.println(e);
+    }
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+	}
+}
+
