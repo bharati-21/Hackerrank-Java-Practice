adf2e1ccef6600194e74e8f60a1a5c93f244526d
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-05-31 22:16:27 +0530

    Created Problem7.java

---
+++ Problem7.java
@@ -0,0 +1,27 @@
+/*
+Problem Link : https://www.hackerrank.com/challenges/java-loops/problem?h_r=next-challenge&h_v=zen
+*/
+
+// Solution :
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int ans=a;
+            for(int j = 0; j<n; j++) {
+                ans+=(int)Math.pow(2,j)*b;
+                System.out.print(ans + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
