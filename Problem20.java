e9cba63f8f99d61860c8bb588ccbde6033c9bbd8
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-01 18:20:06 +0530

    Updated Problem20.java

--- Problem20.java
+++ Problem20.java
@@ -4,6 +4,7 @@ Problem Link : https://www.hackerrank.com/challenges/java-string-tokens/problem?
 
 // Solution :
 
+
 import java.io.*;
 import java.util.*;
 
@@ -11,13 +12,23 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-        String[] sArray = s.split("[, ?.@_'!]+");
-        System.out.println(sArray.length);
+        String[] sArray = s.trim().split("[ !,?._'@]+");
+        if(s == null || s.equals("") || s.trim().equals("") || s.length()==0) {
+            System.out.println("0");
+        }
+        else if(s.length() > 400000) {
+            return;
+        }
+        else{
+            System.out.println(sArray.length);
+        }
+
         for(String str : sArray) {
             System.out.println(str);
         }
         scan.close();
-
     }
 }
+
+
 
