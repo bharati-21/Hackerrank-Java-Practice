43c6e6a4eac156fdccd3820c8e737094626d9212
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-01 11:07:26 +0530

    Created Problem18.java

---
+++ Problem18.java
@@ -0,0 +1,26 @@
+/*
+Problem Link : https://www.hackerrank.com/challenges/java-string-reverse/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+*/
+
+// Solution : 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        int i=0, j=A.length()-1;
+        for(; i<=j; i++, j--) {
+            if(A.charAt(i)!=A.charAt(j))
+                break;
+        }
+        if(i>j)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
+
+
+
