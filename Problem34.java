fe279fded01be2c372e6392ad3357bf017b6fb83
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-02 19:55:08 +0530

    Created Problem34.java

---
+++ Problem34.java
@@ -0,0 +1,36 @@
+/*
+Problem Link : https://www.hackerrank.com/challenges/java-arraylist/problem
+*/
+
+// Solution :
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 0;
+        ArrayList<ArrayList<Integer>> nums = new ArrayList<ArrayList<Integer>>();
+        while(i<n) {
+            int d = s.nextInt();
+            nums.add(new ArrayList<Integer>());
+            while(d-->0) {
+                nums.get(i).add(s.nextInt());
+            }
+            i++;
+        }
+        int q = s.nextInt();
+        while(q-- > 0) {
+            int x = s.nextInt() - 1;
+            int y = s.nextInt() - 1;
+            if(nums.get(x).size()<=y) {
+                System.out.println("ERROR!");
+            }
+            else {
+                System.out.println(nums.get(x).get(y));
+            }
+        }
+        
+    }
+}
