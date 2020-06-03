9d5168649820b50dd3fe0beba0c70333a93a5e49
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-03 18:56:58 +0530

    Created Problem47.java

---
+++ Problem47.java
@@ -0,0 +1,34 @@
+/*
+Problem Link : https://www.hackerrank.com/challenges/java-bigdecimal/problem
+*/
+
+// Solution :
+import java.math.BigDecimal;
+import java.util.*;
+
+class Problem47 {
+    public static void main(String []args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++) {
+            s[i]=sc.next();
+        }
+        sc.close();
+        Arrays.sort(s, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1 == null || s2 == null) {
+                    return 0;
+                }
+                BigDecimal bd1 = new BigDecimal(s1);
+                BigDecimal bd2 = new BigDecimal(s2);
+                return bd2.compareTo(bd1);
+            }
+        });
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
