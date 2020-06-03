ba0ac20f50965e7cdd7846476bd2bc18ad1d9cfe
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-03 09:46:09 +0530

    Updated Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -3,8 +3,7 @@ Problem Link : https://www.hackerrank.com/challenges/welcome-to-java/problem
 */
 
 //Solution :
-
-public class Solution {
+public class Problem1 {
   public static void main(String[] args) {
         System.out.println("Hello, World.\nHello, Java.");
     }
