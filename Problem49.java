4baa0210e0c447e2608648bf4bf0425eea14818c
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-03 19:12:56 +0530

    Created Problem49.java

---
+++ Problem49.java
@@ -0,0 +1,21 @@
+/*
+Problem Link : https://www.hackerrank.com/challenges/java-1d-array-introduction/problem
+*/
+
+// Solution :
+import java.util.*;
+
+public class Problem49 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i<n; i++ ) {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
