1a2c47a7ea529eb9cc7b323d58003232c65ac1c6
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-04 21:15:20 +0530

    Updated Problem22.java

--- Problem22.java
+++ Problem22.java
@@ -7,7 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 
-class Solution {
+class Problem22 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
