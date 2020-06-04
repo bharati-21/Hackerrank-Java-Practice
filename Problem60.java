466618d68db2188a7351041b22fc7f13e157767c
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-04 19:00:27 +0530

    Created Problem60.java

---
+++ Problem60.java
@@ -0,0 +1,20 @@
+/*
+Problem Link : https://www.hackerrank.com/challenges/java-singleton/problem
+*/
+
+// Solution :
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+class Singleton {
+    public  String str;
+    private Singleton() {
+    }
+    public static Singleton getSingleInstance() {
+        return new Singleton();
+    }
+}
