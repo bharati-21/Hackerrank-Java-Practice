7a4c7b3d96710c9631dc627c3ff81f5502d6eccf
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-04 21:24:15 +0530

    Updated Problem9.java

--- Problem9.java
+++ Problem9.java
@@ -7,7 +7,7 @@ Problem Link : https://www.hackerrank.com/challenges/java-end-of-file/problem?h_
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Problem9 {
   public static void main(String[] args) {
         int i = 1;
         Scanner s = new Scanner(System.in);
