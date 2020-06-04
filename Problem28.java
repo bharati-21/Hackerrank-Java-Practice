383b3efe91feaa585190844a1a14e2ab51c19954
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-04 21:18:25 +0530

    Updated Problem28.java

--- Problem28.java
+++ Problem28.java
@@ -18,7 +18,7 @@ class MyCalculator implements AdvancedArithmetic {
         return sum;
     }
 }
-class Solution {
+class Problem28 {
     public static void main(String []args) {
         MyCalculator my_calculator = new MyCalculator();
         System.out.print("I implemented: ");
