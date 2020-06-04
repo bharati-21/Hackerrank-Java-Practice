0d4b32e89db8b37e2ed5e325c1fe5f32935ade70
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-04 20:54:10 +0530

    Updated Problem11.java

--- Problem11.java
+++ Problem11.java
@@ -9,7 +9,6 @@ import java.security.*;
 
 public class Problem11 {
     public static void main(String[] args) {
-        DoNotTerminate.forbidExit();
         try {
             Scanner in = new Scanner(System.in);
             int n = in .nextInt();
@@ -28,20 +27,4 @@ public class Problem11 {
     }
 }
 
-class DoNotTerminate {
-    public static class ExitTrappedException extends SecurityException {
-        private static final long serialVersionUID = 1;
-    }
-    public static void forbidExit() {
-        final SecurityManager securityManager = new SecurityManager() {
-        @Override
-            public void checkPermission(Permission permission) {
-                if (permission.getName().contains("exitVM")) {
-                    throw new ExitTrappedException();
-                }
-            }
-        };
-        System.setSecurityManager(securityManager);
-    }
-}
 
