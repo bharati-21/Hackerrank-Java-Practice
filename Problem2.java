ed33fb88b2f3925ba75b0b51779bf58402d7184b
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2020-06-04 21:14:10 +0530

    Updated Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -6,7 +6,7 @@ Problem Link : https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/pro
 
 import java.util.*;
 
-public class Solution {
+public class Problem2 {
   public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
